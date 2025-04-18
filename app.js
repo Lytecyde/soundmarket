de6227294b803089e46d96ba65d3306a5db2431c
Tanel Lebedev <tanel.lebedev@gmail.com>
2025-04-18 14:23:23 +0300

    Data is loading

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // Using pure JS, after page has loaded, load the data from the JSON file
 document.addEventListener("DOMContentLoaded", function() {
     // Fetch the JSON data
-    fetch('data.json')
+    fetch('https://raw.githubusercontent.com/tanel/soundmarket/refs/heads/main/msci_emerging_markets_converted.json')
         .then(response => response.json())
         .then(data => {
             // Call the function to display the data
-            displayData(data);
+            startMusic(data);
         })
         .catch(error => console.error('Error fetching data:', error));
+});
+
+function startMusic() {
+    console.log('Starting sound market...');
 }
